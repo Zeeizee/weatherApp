697c4c986ada31e4603ca0c84bc273975680b7ae
Zeenat Parveen <zenizee563@gmail.com>
2021-05-28 05:10:30 +0500

    HTTPS functionality worked

--- js/forecast.js
+++ js/forecast.js
@@ -3,7 +3,7 @@ const apiKey = 'rpP1ZAAfdIsaW7tWC88ZKh9bmquoMLb4';
 
 const getCityInfo = async (city) => {
     // city bse url get from accweatherapp
-    const baseurl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const baseurl = 'https://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${apiKey}&q=${city}`;
     const cityinfo = await fetch(baseurl + query);
     const data = await cityinfo.json();
@@ -17,7 +17,7 @@ const getCityInfo = async (city) => {
 
 const weatherinfo = async (citycode) => {
 
-    const baseurl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const baseurl = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${citycode}?apikey=${apiKey}`;
     const weatherinfo = await fetch(baseurl + query);
     const data = await weatherinfo.json();
